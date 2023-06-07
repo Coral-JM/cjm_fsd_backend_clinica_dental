c3a8efd2f38db9c0891913c9d97f3fe2e3a027e5
Coral Jimenez <info.coraljimenez@gmail.com>
2023-06-07 13:43:45 +0200

    Feat: Upload sequelize and db

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const db = require('./db');
+const router = require('./router');
+
 
 const PORT = 5000;
+
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Servidor levantado en puerto ${PORT}`);
 
+app.use(router);
+
+db.then(() =>
+    {
+        app.listen(PORT, () => {
+            console.log('Server is running on port: ' + PORT);
+        })
+    }
+).catch((error) => {
+    console.error('Error starting server', error.message);
 })
-
