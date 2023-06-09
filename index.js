ffe1255be470febcaed0accfa1f23f90e5963d78
Coral Jimenez <info.coraljimenez@gmail.com>
2023-06-09 19:53:57 +0200

    Feat: user and role model and migration. Fix: email user and role type unique

--- index.js
+++ index.js
@@ -3,16 +3,42 @@ const app = express();
 const db = require('./db');
 const router = require('./router');
 
+const { User } = require('./models');
+const bcrypt = require('bcrypt');
+
+
 
 const PORT = 5000;
 
 app.use(express.json());
 
+app.post('/register', async (req, res) => {
+    
+    try {
+        if (req.body.password.length < 4) {
+            return res.send('Password must be longer than four characters')
+        }
+
+        const newUser = await User.create(
+            {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                role_id: 1
+            }
+        )
+        return res.send(newUser);
+    } catch (error) {
+        return res.send('Somethign went wrong creating users' + error.message)
+        
+    }
+})
 
 app.use(router);
 
 app.get('/health', (req, res) => {
     return res.send('healthy');
+    
 });
 
 db.then(() =>
--- migrations/20230607145807-create-user.js
+++ migrations/20230607145807-create-user.js
@@ -13,13 +13,18 @@ module.exports = {
         type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       password: {
         type: Sequelize.STRING
       },
       role_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Roles",
+          key: "id"
+        }
       },
       createdAt: {
         allowNull: false,
