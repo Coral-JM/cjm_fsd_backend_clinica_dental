d6af3b15bdec1830dbdc5f146761f7007e5d6cc3
Coral Jimenez <info.coraljimenez@gmail.com>
2023-06-09 20:32:01 +0200

    Feat: Register abstraction to authController

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
+const authController = {};
+
+authController.register = async (req, res) => {
+    
+    try {
+        if (req.body.password.length < 4) {
+            return res.send('Password must be longer than four characters')
+        }
+        const newPassword = bcrypt.hashSync(req.body.password, 8);
+
+        const newUser = await User.create(
+            {
+                name: req.body.name,
+                email: req.body.email,
+                password: newPassword,
+                role_id: 1
+            }
+        )
+        return res.send(newUser);
+    } catch (error) {
+        return res.send('Somethign went wrong creating users' + error.message)
+        
+    }
+}
+
+module.exports = authController
