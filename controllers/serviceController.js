a13d372420f6fed38c7bffe56237e0c1d3531d1c
Coral Jimenez <info.coraljimenez@gmail.com>
2023-06-10 17:18:38 +0200

    Fix: Service controller modified

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,61 +1,10 @@
-const { Service } = require("../models");
-const authController = require('./authController');
 const serviceController = {};
 
-serviceController.createService = async(req, res) => {
-    try {
-        const { name, description } = req-body;
-
-        const newService = await Service.create(
-            {
-                name: name,
-                description
-            }
-        )
-        return res.json({
-            success: true,
-            message: "Service created",
-            data: newService
-        })
-    } catch (error) {
-        return res.status(500).json({
-            success: false, 
-            message: "Service can't be created",
-            error: error
-        }
-        )
-        
-    }
+serviceController.getServices = (req, res) => {
+    return res.send('Services')
 }
-
-serviceController.updateService = async(req, res) => {
-    try {
-        const serviceId = req.params.id;
-        const service = await Service.findbyPk(serviceId);
-
-        if (!service) {
-            return res.json({
-                success: true, 
-                message: "Service doesn't exist"
-            })
-        }
-        const { name, description } = req.body;
-        const serviceUpdated = await Service.update(
-            {
-                name: name, 
-                description: description
-            },
-            {
-                where: { id: serviceId}
-            })
-            return res.json({
-                success: true,
-                message: "Service updated",
-                data: serviceUpdated
-            })
-    } catch (error) {
-        
-    }
+serviceController.createServices = (req, res) => {
+    return res.send('Create Services')
 }
 
 module.exports = serviceController;
